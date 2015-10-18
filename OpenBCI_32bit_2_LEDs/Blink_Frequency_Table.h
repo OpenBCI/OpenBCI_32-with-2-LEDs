09c1a435bf3d23041036375c687bf177e81c725b
Joel Murphy <biomurph@gmail.com>
2015-10-18 15:08:12 -0400

    Update Blink_Frequency_Table.h

--- OpenBCI_32bit_2_LEDs/Blink_Frequency_Table.h
+++ OpenBCI_32bit_2_LEDs/Blink_Frequency_Table.h
@@ -3,7 +3,7 @@
  *    
  *      sampleCounts = 1000x((1/freq)/2)/4
  *      
- *    where sampleCounts = modulo, freq = blink frequency 
+ *    where sampleCounts = ADS sample counts every 4mS, freq = blink frequency 
  *    
  *      Example:
  *      25 = 1000x((1/5)/2)/4
@@ -14,17 +14,17 @@
  * 
  *  Here's a list of (some) possible blink frequencies
  * 
- *  modulo    |   frequency in Hz
- *  
- *     5      |     25
- *    10      |     12.5
- *    15      |     8.33
- *    20      |     6.25
- *    25      |     5
- *    30      |     4.16
- *    35      |     3.57
+ *  sampleCounts    |   frequency in Hz
+ *                  |
+ *           5      |     25
+ *          10      |     12.5
+ *          15      |     8.33
+ *          20      |     6.25
+ *          25      |     5
+ *          30      |     4.16
+ *          35      |     3.57
  * 
- *  Note: the modulo number is used in the loop to switch the state of the LEDs
+ *  Note: the sampleCounts number is used as modulo in the loop to switch the state of the LEDs
  *  
  *  
  *  
